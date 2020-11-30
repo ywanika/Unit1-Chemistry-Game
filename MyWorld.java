1ebacabd8210867d1f27a14097d9da1ab9cc835e
shreyachandra1 <44423818+shreyachandra1@users.noreply.github.com>
2020-11-30 09:06:18 -0800

    yay comments

--- MyWorld.java
+++ MyWorld.java
@@ -3,15 +3,15 @@ import java.util.List;
 import java.util.ArrayList; // allows us to make lists 
 
 /**
- * Write a description of class MyWorld here.
+ * Develops the world for the electron game, and places the necessary actors into it.
  * 
  * Shreya Chandra, Anika Sharma , Niru Suravarjjala, Jenny Wang 
  * 11/23/20
  */
 public class MyWorld extends World
 {
-    public String deckChoice = "Oxygen";
-    public int numElec;
+    public String deckChoice = "Oxygen"; //develops a variable that can be modified based on level
+    public int numElec; //will be used to instantiate number of electrons
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -21,46 +21,48 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         
-        EnergyBar energyBar = new EnergyBar();
+        //Creates the energy bar on the left side of the world
+        EnergyBar energyBar = new EnergyBar(); 
         addObject(energyBar, 20, 170);
         
+        //Create the arrow within the energy bar
         EnergyArrow energyArrow = new EnergyArrow();
         addObject(energyArrow, 20, 290);
         
+        //Creates the cannon and places it in the bottom left corner of the screen
         Canon canon = new Canon();
         addObject(canon, 70, 350);
         
+        //Shows the current energy
         showText("Energy (kj/mol)", 85,20);
         
+        //Shows the value markers for the energy bar
         showText("4000", 50,50);
         showText("3000", 50,110);
         showText("2000", 50,170);
         showText("1000", 50,230);
         showText("0", 50,290);
         
-        // adding Bohr model's rings and 
+        // Adds Bohr model's rings 
         BohrRing1 ring1 = new BohrRing1();
         addObject(ring1, 400, 200); 
         
         BohrRing2 ring2 = new BohrRing2();
         addObject(ring2, 400, 200); 
         
+        //Places the nucleus in the middle of the Bohr rings
         Nucleus nucleus = new Nucleus(); 
         addObject(nucleus, 390,190); 
         
+        //Creates the next button in order for levels to change
         NextButton next = new NextButton();
         addObject(next, 560, 30); 
         
-       
+        //Starts the game
         startGame();
     }
     public void startGame(){
-        //List<String> gameDeck = new ArrayList<String>(); 
-        //gameDeck.add(deckChoice); // add 2 electrons 
-        //gameDeck.add("Fluorine-FluorideIon"); // add 1 electron 
-        //gameDeck.add("Nitrogen-CyanideIon"); // add 1 electron to N 
-        //System.out.println("ArrayList : " + gameDeck.toString()); // debug
-        //for (int i=0; i < gameDeck.size(); i++) {
+        //Changes the number of electrons based on what level the game is on
         if (deckChoice == "Oxygen"){
             numElec = 8; 
         }
@@ -70,14 +72,15 @@ public class MyWorld extends World
         else if (deckChoice == "Nitrogen"){
             numElec = 7; 
         }
-        setUp();
-        //}
+        setUp(); //sets up the chosen number of electrons
     }
     public void setUp()
     {   
-        removeObjects(getObjects(Electron.class));
+        removeObjects(getObjects(Electron.class)); //refreshes the electrons 
+                                                  //(instantiates new ones)
         
-        Electron electron = new Electron(); 
+        Electron electron = new Electron(); //creates a brand new electron
+        //Places electrons in specific positions based on total number of electrons
         for (int i=0; i<numElec; i++) {
             if (i==0) {
                 addObject(new Electron(), 385, 85);
